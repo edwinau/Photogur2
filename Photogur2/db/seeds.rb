7d63b3582ef7634813163c71f55d38d9b27aab58
EDWIN AU <edwinau@rogers.com>
2017-04-16 12:15:00 -0400

    photos show

--- Photogur2/db/seeds.rb
+++ Photogur2/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Photo.delete_all
+
+Photo.create(
+  title:  'The old church on the coast of White sea',
+  artist: 'Sergey Ershov',
+  url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg'
+)
+
+Photo.create(
+  title:  'Sea Power',
+  artist: 'Stephen Scullion',
+  url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg'
+)
+
+Photo.create(
+  title:  'Into the Poppies',
+  artist: 'John Wilhelm',
+  url:    'http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg'
+)
